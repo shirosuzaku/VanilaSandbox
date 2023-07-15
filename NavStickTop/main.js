161fd3087444050ba49d5c8acaffbc2bb7114b62
shirosuzaku <118347073+shirosuzaku@users.noreply.github.com>
2023-07-15 13:04:49 +0300

    Add files via upload

---
+++ NavStickTop/main.js
@@ -0,0 +1,27 @@
+const nav = document.querySelector('.nav')
+
+let navY
+
+let x = false
+let y = x
+
+window.addEventListener('scroll',()=>{
+
+    // navY = nav.getClientRects()[0].y
+
+    // if(navY < 0 ){
+    //     x = true
+    // }else{
+    //     x = false
+    // }
+
+    // if(x != y){
+    //     // navY < 0 ? console.log("d") : console.log("u")
+    //     navY < 0 
+    //     ? nav.classList.add('sticky')
+    //     : nav.classList.remove('sticky')
+    // }
+
+    // y = x
+
+})
